5622cedf9e6e4947f58bd55a62eeac81ac5d5e53
Jonathan Castillo <jcastillo@kobu.io>
2024-04-23 23:24:04 -0600

    users crud completed

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UserDTO } from '../dto/users.dto';
+import { UserDTO, UserUpdateDTO } from '../dto/users.dto';
 
 @Controller('users')
 export class UsersController {
@@ -12,6 +12,26 @@ export class UsersController {
     public async registerUser(
         @Body() body: UserDTO
     ) {
-        return await this.usersService.createUser(body);
+        return await this.usersService.registerUser(body);
+    }
+
+    @Get('all')
+    public async findAllUsers(){
+        return await this.usersService.findAllUsers();
+    }
+    
+    @Get(':userId')
+    public async findUserById(@Param('userId') userId: string) {
+        return await this.usersService.findUserById(userId);
+    }
+    
+    @Put('edit/:userId')
+    public async updateUser(@Body() body: UserUpdateDTO, @Param('userId') userId: string) {
+        return await this.usersService.updateUser(body, userId);
+    }
+    
+    @Delete('delete/:userId')
+    public async deleteUser(@Param('userId') userId: string) {
+        return await this.usersService.deleteUser(userId);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { UserDTO } from '../dto/users.dto';
+import { UserDTO, UserUpdateDTO } from '../dto/users.dto';
 import { UsersEntity } from '../entities/users.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { ErrorManager } from 'src/utils/error.util';
 
 @Injectable()
 export class UsersService {
@@ -12,14 +13,67 @@ export class UsersService {
         private readonly userRepository: Repository<UsersEntity>
     ){}
 
-    public async createUser(body: UserDTO): Promise<UsersEntity> {
+    public async registerUser(body: UserDTO): Promise<UsersEntity> {
         try {
             const hashedPassword = await bcrypt.hash(body.password, 10);
             body.password = hashedPassword;
             return await this.userRepository.save(body);
         } catch (error) {
-            console.log(error);
-            
+            throw ErrorManager.createSignatureError(error.message);
         }
     }
+
+    public async findAllUsers(): Promise<UsersEntity[]> {
+        try {
+            const users = await this.userRepository.find();
+            if(users.length === 0) throw new ErrorManager({ type: 'NOT_FOUND', message: 'No users found' });
+            return users;
+        } catch (error) {
+            throw ErrorManager.createSignatureError(error.message);
+        }
+    }
+
+    public async findBy({ key, value }: {
+        key: keyof UserDTO;
+        value: any;
+    }) {
+        try {
+            const user: UsersEntity = await this.userRepository.createQueryBuilder('user').addSelect('user.password').where({ [key]: value }).getOne();
+            return user;
+        } catch (error) {
+            throw ErrorManager.createSignatureError(error.message);
+        }
+    }
+   
+    public async findUserById(userId: string): Promise<UsersEntity> {
+        try {
+            const user = await this.userRepository.createQueryBuilder('user').where({ id: userId }).getOne();
+            if(!user) throw new ErrorManager({ type: 'NOT_FOUND', message: 'User not found' });
+            return user;
+        } catch (error) {
+            throw ErrorManager.createSignatureError(error.message);
+        }
+    }
+    
+    public async updateUser(body: UserUpdateDTO,userId: string): Promise<UpdateResult | undefined> {
+        try {
+            const updatedUser: UpdateResult = await this.userRepository.update(userId, body);
+            if(updatedUser.affected === 0) throw new ErrorManager({ type: 'BAD_REQUEST', message: 'Update User failed' });
+            return updatedUser;
+        } catch (error) {
+            throw ErrorManager.createSignatureError(error.message);
+        }
+    }
+
+    public async deleteUser(userId: string): Promise<DeleteResult | undefined> {
+        try {
+            const deletedUser: DeleteResult = await this.userRepository.delete(userId);
+            if(deletedUser.affected === 0) throw new ErrorManager({ type: 'BAD_REQUEST', message: 'Delete User failed' });
+            return deletedUser;
+        } catch (error) {
+            throw ErrorManager.createSignatureError(error.message);
+        }
+    }
+
+
 }
